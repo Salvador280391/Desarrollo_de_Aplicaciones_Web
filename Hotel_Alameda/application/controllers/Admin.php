657bd8e504e3c3eb1a9530261d691e4398868a33
Salvador280391 <salvadortoral@gmail.com>
2019-08-02 02:45:10 -0500

    se inserta cabecera de código
    se crea la cabecera del código y se realizan cambios ya que la base de datos fue rediseñada eliminando la tabla de Cliente.

--- Hotel_Alameda/application/controllers/Admin.php
+++ Hotel_Alameda/application/controllers/Admin.php
@@ -1,3 +1,18 @@
+<?php
+
+/**
+ * @category   Controllers
+ * @package    Admin
+ * @author     Salvador Toral Naranjo
+ * @version    1.0
+ * @link       git@github.com:Salvador280391/Desarrollo_de_Aplicaciones_Web.git
+ * Viernes 02 de Agosto del 2019
+ * 
+ * Se actualizo la cabecera de código
+ */
+
+?>
+
 <?php 
 
 class Admin extends CI_Controller
@@ -12,20 +27,24 @@ class Admin extends CI_Controller
             $crud = new grocery_CRUD();
 
             $crud->set_theme('bootstrap-v4');
-            $crud->set_table('Reservacion');
+            $crud->set_table('DetalleReservacion');
             $crud->set_subject('Reservación');
-            $crud->columns('fechaLlegada','fechaSalida','Cliente_idCliente');
-            $crud->required_fields('fechaLlegada','fechaSalida');
-            $crud->display_as('fechaLlegada','Fecha de llegada');
-            $crud->display_as('fechaSalida','Fecha de salida');
+            $crud->columns('Reservacion_idReservacion','Habitacion_numHabitacion');
+            $crud->display_as("Habitacion_numHabitacion","Habitación");
+            $crud->display_as("Reservacion_idReservacion","Fecha de Reservación");
+            $crud->display_as("totalR","Total");
 
-            $crud->set_relation("Cliente_idCliente","Cliente","nombreCl");
-            $crud->display_as("Cliente_idCliente","Cliente");
 
+            $crud->set_relation('Habitacion_numHabitacion','Habitacion','{descripcionH}  {precio}');
+            $crud->set_relation('Reservacion_idReservacion','Reservacion','LLegada {fechaLlegada} --- Salida {fechaSalida}');            
+            
             $output = $crud->render();
-            $this->load->view('index.php',(array)$output);
+            $this->load->view('Administrador/head.php');
+            $this->load->view('Administrador/index.php',(array)$output);
+            $this->load->view('Administrador/footer.php');
         }catch(Exception $ex){
               show_error($ex->getMessage().' ----- '.$ex->getTraceAsString());
         }
     }
+
 }
